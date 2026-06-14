e05a7c769cd1b4ddc6eea74fb07523b291dcd3fb
Jason Smith <jasonsmith@Jasons-MacBook-Air.local>
2026-06-14 11:00:14 -0400

    Authoring test captures and snapshot data from region/capture testing

---
+++ src/snapshots/Alo/index7.js
@@ -0,0 +1,10 @@
+import * as runtime from "runtime";
+
+namespace `snapshots` 
+(
+    class Alo extends runtime.Controller {
+        async onConnected() {
+            await super.onConnected();
+        }
+    }
+);
---
+++ src/snapshots/Google/index14.js
@@ -0,0 +1,10 @@
+import * as runtime from "runtime";
+
+namespace `snapshots` 
+(
+    class Google extends runtime.Controller {
+        async onConnected() {
+            await super.onConnected();
+        }
+    }
+);
